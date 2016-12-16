3feb2feb87fe4f7d85aa5a4ebfd50f18050dc0dd
yanqizhao <yanqizhao@126.com>
2016-12-16 14:17:50 +0800

    day2

---
+++ SwiftDay-2/SwiftDay-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDay-2
+//
+//  Created by zhaoyq on 2016/12/16.
+//  Copyright © 2016年 zhaoyq. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
